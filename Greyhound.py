496171e9e71ead65f50d91570a9d51f016e74fe1
antoine grosnit <antoine.grosnit70@gmail.com>
2018-05-05 14:48:58 +0200

    Début du script

---
+++ Greyhound.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+##
+
+racine='C:/Users/antoine/Desktop/Polytechnique/Binet/X Finance/Sports bet'
+
+data=pd.read_csv(racine +'/greyhoundwin_18.04.28_18.05.04.csv')
+
+greyhound=data.groupby
+
+def dist(s):
+    a=s[0]
+    
+
+df=data.groupby('EVENT_ID')
+
