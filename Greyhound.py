fd67cb64888f1b5db52b3b7801cb44c413d4d4dd
antoine grosnit <antoine.grosnit70@gmail.com>
2018-05-05 18:30:15 +0200

    Repartir sur des bases seines

--- Greyhound.py
+++ Greyhound.py
@@ -5,35 +5,24 @@ import os
 import os.path
 
 ##
-#racine='C:/Users/antoine/Desktop/Polytechnique/Binet/X Finance/Data Sports bet'
-
-
-#racine='C:/Users/antoine/Desktop/Polytechnique/Binet/X Finance/Sports bet'
+#racine='C:/Users/antoine/Desktop/Polytechnique/Binet/X Finance/Data Sports bet/'
 #racine="/Users/yassinhamaoui/Desktop/data_sports_bets/"
 
 data=pd.read_csv(open(racine+'test.csv',encoding='utf-8'),index_col=0)
-
-event_winner=data.groupby("event_id").apply(lambda x: np.repeat(x["racer_id"].values,x["win_lose"]))
-    
-
 data=pd.read_csv(open(racine+'/test.csv'),index_col=0)
 data=data[data.columns[:7]]
 data.index.name='event_id'
 
+##
+event_winner=data.groupby("event_id").apply(lambda x: np.repeat(x["racer_id"].values,x["win_lose"]))
 
 ##
-
-
 racerAux=data.groupby('racer_id')
-
 racer=pd.DataFrame(columns=['event_id','win_lose'])
-
 racer['win_lose']=racerAux['win_lose'].apply(lambda x:np.array(x))
 racer['event_id']=racerAux.apply(lambda x:np.array(x.index))
  
 ##
-
-
 def strategie(event_id,nCourse=10):
     listRacer=np.array(data.loc["event_id"]["racer_id"])
     for racer in listRacer:
