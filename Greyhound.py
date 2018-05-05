bd35c56148a69c0974f08b1a35af79da54c18abb
antoine grosnit <antoine.grosnit70@gmail.com>
2018-05-05 14:56:24 +0200

    test210

--- Greyhound.py
+++ Greyhound.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 ##
 
 racine='C:/Users/antoine/Desktop/Polytechnique/Binet/X Finance/Sports bet'
 
-data=pd.read_csv(racine +'/greyhoundwin_18.04.28_18.05.04.csv')
+data=pd.read_csv(open(racine+'/greyhoundwin_18.04.28_18.05.04.csv'),index_col=0)
 
 greyhound=data.groupby
+
+print('bonjour')
 
 def dist(s):
     a=s[0]
