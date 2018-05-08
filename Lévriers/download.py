802ca04f81f0b7719770ad8bb44c5d94a4b4f62b
Romain Fouilland <romain.fd@gmail.com>
2018-05-08 11:42:28 +0200

    Python to download data is working

--- Lévriers/download.py
+++ Lévriers/download.py
@@ -1,9 +1,9 @@
 import requests
 import datetime
 
-dataType = 'place'
-debut = datetime.datetime(2012, 9, 20)
-end = debut.today()
+dataType = 'win'
+debut = datetime.datetime(2013, 1, 1)
+fin = debut.today()
 
 def dateToString(date):
 	return zeroIfOneDigit(date.day)+zeroIfOneDigit(date.month)+str(date.year);
@@ -14,13 +14,14 @@ def zeroIfOneDigit(int):
 	return str(int)
 
 d = debut
-data = requests.get('http://www.betfairpromo.com/betfairsp/prices/dwbfgreyhound'+dataType+""+dateToString(d)+'.csv')
-print(data.text)
-d = debut
-with open("output.csv") as file:
-	while (d < end):
+with open("output_"+dataType+"_"+dateToString(debut)+"_"+dateToString(fin)+".csv", "w", encoding='utf-8') as file:
+	file.write("event_id,place,event_name,date,racer_id,racer_name,win_lose,BSP,PPWAP,MORNINGWAP,PPMAX,PPMIN,IPMAX,IPMIN,MORNINGTRADEDVOL,PPTRADEDVOL,IPTRADEDVOL\n")
+	while (d < fin):
 		data = requests.get('http://www.betfairpromo.com/betfairsp/prices/dwbfgreyhound'+dataType+""+dateToString(d)+'.csv')
 		if data.status_code == 200:
-			print(dateToString(d))
-			#print(data.text)
+			tab = data.text[163:]
+			file.write(tab)
+			print(dateToString(d)+ " done.")
 		d = d + datetime.timedelta(1)
+	print("Done.")
+
