ee339c8b0b6549ee91e6d2cc234d00afbdb35345
Romain Fouilland <romain.fd@gmail.com>
2018-05-10 12:34:27 +0200

    Integration of the data download to the core data analysis script

--- Greyhound.py
+++ Greyhound.py
@@ -3,12 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import os.path
+from Greyhounds.download import *
 
-##
+racine = "C:/Users/Romain Fouilland/Documents/Romain/Travail/Polytechnique/Binets/X Finance/Horse racing/"
 #racine='C:/Users/antoine/Desktop/Polytechnique/Binet/X Finance/Data Sports bet/'
 #racine="/Users/yassinhamaoui/Desktop/data_sports_bets/"
 
-data=pd.read_csv(open(racine+'test.csv',encoding='utf-8'),index_col=0)
+## PARAMETERS BUILDING
+# win or place ?
+dataType = 'place'
+# start and end dates (year, month, day)
+debut = datetime.datetime(2018, 5, 1)
+fin = datetime.datetime(2018, 5, 5)
+## /PARAMETERS
+
+# Télécharge les données voulues
+def openData(dataType, debut, fin):
+    # if the file already exists we do not download it again
+    if (os.path.isfile(racine+"Greyhounds/Output/output_"+dataType+"_"+dateToString(debut)+"_"+dateToString(fin)+".csv")):
+        print("File already exists. Using existing data.")
+        return racine+"Greyhounds/Output/output_"+dataType+"_"+dateToString(debut)+"_"+dateToString(fin)+".csv"
+    return getData(racine+"Greyhounds/", dataType, debut, fin)
+
+data=pd.read_csv(open(openData(dataType, debut, fin),encoding='utf-8'),index_col=0)
 data=data[data.columns[:7]]
 data.index.name='event_id'
 
@@ -50,3 +67,5 @@ def backTest(strat):
         if strat(i) in eventWinner.loc[i]:
             w+=1
     return w/len(s)
+
+print(backTest(strategie))
--- Greyhounds/download.py
+++ Greyhounds/download.py
@@ -1,10 +1,10 @@
 import requests
 import datetime
+print("Start importing download.py")
 
-
-## TEST LOCAL
+## PARAMETERS BUILDING
 # win or place ?
-dataType = 'win'
+dataType = 'place'
 # start and end dates (year, month, day)
 debut = datetime.datetime(2018, 5, 1)
 fin = debut.today()
@@ -23,10 +23,10 @@ def zeroIfOneDigit(int):
 # Core of the program
 # Downloads the data from betfair for every day between start and end dates (if available)
 # Stores the data of each (minus the row of column names) in a gloabl csv file in a folder outside of the git repo (called Data Horse Racing and at the same level)
-def getData(dataType, debut, fin):
+def getData(OutputFolderPath, dataType, debut, fin):
 	d = debut
 	cpt = 0
-	with open("output/output_"+dataType+"_"+dateToString(debut)+"_"+dateToString(fin)+".csv", "w", encoding='utf-8') as file:
+	with open(OutputFolderPath+"Output/output_"+dataType+"_"+dateToString(debut)+"_"+dateToString(fin)+".csv", "w", encoding='utf-8') as file:
 		file.write("event_id,place,event_name,date,racer_id,racer_name,win_lose,BSP,PPWAP,MORNINGWAP,PPMAX,PPMIN,IPMAX,IPMIN,MORNINGTRADEDVOL,PPTRADEDVOL,IPTRADEDVOL\n")
 		while (d < fin):
 			data = requests.get('http://www.betfairpromo.com/betfairsp/prices/dwbfgreyhound'+dataType+""+dateToString(d)+'.csv')
@@ -37,6 +37,6 @@ def getData(dataType, debut, fin):
 				print(dateToString(d)+ " done.")
 			d = d + datetime.timedelta(1)
 		print("Done: {} data-days have been downloaded between the dates of {} and {}".format(cpt, debut.strftime("%d-%m-%y"), fin.strftime("%d-%m-%y")))
-	return "Lévriers/output/output_"+dataType+"_"+dateToString(debut)+"_"+dateToString(fin)+".csv"
+	return OutputFolderPath+"Output/output_"+dataType+"_"+dateToString(debut)+"_"+dateToString(fin)+".csv"
 
-getData(dataType, debut, fin)
+print("download.py correctly imported.")
