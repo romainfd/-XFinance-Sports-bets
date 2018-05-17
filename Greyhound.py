3edcfd4b77f3ef8ed95c976d14a18ffed0c5d6e6
Romain Fouilland <romain.fd@gmail.com>
2018-05-17 21:33:56 +0200

    Lower Complexity

--- Greyhound.py
+++ Greyhound.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 import os
 import os.path
 import Greyhounds.download as dnld
+import time
 
+## PARAMETRES GLOBAUX
+minRacersNb = 2 # nb of racer min to bet
+
+## /PARAMETRES
 
 ##
 racine = "C:/Users/Romain Fouilland/Documents/Romain/Travail/Polytechnique/Binets/X Finance/Horse racing/"
@@ -16,7 +21,7 @@ dataPath = "Greyhounds/Output/"
 # win or place ?
 dataType = 'place'
 # start and end dates (year, month, day)
-debut = dnld.datetime.datetime(2018, 1, 1)
+debut = dnld.datetime.datetime(2018, 3, 1)
 # fin = debut.today()
 fin = dnld.datetime.datetime(2018, 4, 1)
 ## /PARAMETERS
@@ -34,10 +39,11 @@ data=data[data.columns[:7]]
 data.index.name='event_id'
 data['win_lose']=data['win_lose'].fillna(0).astype(int)
 data['racer_name'] = data['racer_name'].apply(lambda x: x[3:])
-print("Donnéees chargées")
+print("Données chargées")
 
 ##
 eventWinner=data.groupby("event_id").apply(lambda x: np.repeat(x["racer_name"].values, x["win_lose"]))
+courses=data.groupby("event_id").apply(lambda x: np.array(x["racer_name"]))
 
 racerAux=data.groupby('racer_name')
 racer=pd.DataFrame(columns=['event_id','win_lose'])
@@ -48,33 +54,40 @@ print("Données triées")
 ##
 def strategie(event_id,nCourse=10):
     try:
-        listRacer=list(np.array(data.loc[event_id]["racer_name"]))
+        listRacer=courses.loc[event_id]
     except TypeError:
-        listRacer=[data.loc[event_id]["racer_name"]]
+        listRacer=[courses.loc[event_id]]
     maxPerf=0
     bestRacer=listRacer[0]
-    for racerID in listRacer:
-        courses=list(racer.loc[racerID]["event_id"])
-        indexCourse=courses.index(event_id)
-        resultats=list(racer.loc[racerID]["win_lose"])
-        compteur=1
-        performance=0
-        while (indexCourse-compteur>=0 and compteur<=nCourse) :
-            performance+=resultats[indexCourse-compteur]
-            compteur+=1
+    for racerName in listRacer:
+        # On récupere les courses (et les résultats) faites par le racer
+        coursesRacer=racer.loc[racerName]["event_id"]
+        resultats=racer.loc[racerName]["win_lose"]
+        # on se place à la course que l'on veut prédire (on apprend qu'avant)
+        indexCourse=np.where(coursesRacer == event_id)[0][0]
+        # comme les index sont croissants, on récupère directement le bon nb de courses dispos avant avec indexCourse
+        nbCoursesUtilisables = min(indexCourse, nCourse)
+        # initialisation
+        performance = np.sum(resultats[indexCourse - nbCoursesUtilisables:indexCourse])
         if (performance>maxPerf):
             maxPerf=performance
-            bestRacer=racerID
+            bestRacer=racerName
     return bestRacer
 
 def backTest(strat):
     w=0
-    s=set(data.index)
-    for i in s:
-        if strat(i) in eventWinner.loc[i]:
-            w+=1
-    return w/len(s)
+    nbBets = 0
+    # set pour ne traiter qu'une fois chaque liste
+    for i in courses.index:
+        if courses.loc[i].size >= minRacersNb:
+            # on parie sur cette course
+            nbBets += 1
+            if strat(i) in eventWinner.loc[i]:
+                # notre levrier est dans les gagnants
+                w+=1
+    return w/nbBets
 ##♦
 
-
+t0 = time.time()
 print(backTest(strategie))
+print(time.time() - t0)
