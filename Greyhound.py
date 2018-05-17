d96eb174976ac8370da875eb9b6b8978d840b697
Romain Fouilland <romain.fd@gmail.com>
2018-05-18 00:08:58 +0200

    Different strategies

--- Greyhound.py
+++ Greyhound.py
@@ -9,7 +9,9 @@ import datetime
 
 ## PARAMETRES GLOBAUX
 minRacersNb = 2 # nb of racer min to bet
-
+dataPath = "Greyhounds/Output/"
+factorLastRace = 1
+tempFactor = 1
 ## /PARAMETRES
 
 ## CHOIX AUTOMATIQUE DE LA RACINE
@@ -19,8 +21,8 @@ racines = {
     'Users\\yassin': "/Users/yassinhamaoui/Desktop/data_sports_bets/" #a finir de remplir
 }
 racine = racines[os.getcwd()]
+## /RACINE
 
-dataPath = "Greyhounds/Output/"
 ## PARAMETERS BUILDING
 # win or place ?
 dataType = 'place'
@@ -38,29 +40,55 @@ def openData(dataType, debut, fin):
         return racine+dataPath+"output_"+dataType+"_"+debut.strftime("%d%m%Y")+"_"+fin.strftime("%d%m%Y")+".csv"
     return dnld.getData(racine+dataPath, dataType, debut, fin)
 
+# Traitement des données
+    # enleve les entiers . et espace
+def formatName(name):
+    nameInit = name
+    for i in range(len(nameInit)):
+        if (ord(name[0]) >= 65 and ord(name[0]) <= 90):
+            # on est bien sur une majuscule
+            index = name.find("(")
+            if (index >= 0):
+                name = name[:index]
+                if (name[-1] == " "):
+                    name = name[:-1]
+            return name.lower()
+        # sinon, on supprime une lettre
+        name = name[1:]
+    # Commence TJS par une maj => on sort jamais du for
+    print("Erreur sur le nom de cheval : "+nameInit)
+    return nameInit
+
 data=pd.read_csv(open(openData(dataType, debut, fin),encoding='utf-8'),index_col=0)
 data=data[data.columns[:7]]
 data.index.name='event_id'
 data['win_lose']=data['win_lose'].fillna(0).astype(int)
-data['racer_name'] = data['racer_name'].apply(lambda x: x[3:])
+data['racer_name'] = data['racer_name'].apply(lambda x: formatName(x))
 print("Données chargées")
 
 ##
-eventWinner=data.groupby("event_id").apply(lambda x: np.repeat(x["racer_name"].values, x["win_lose"]))
-courses=data.groupby("event_id").apply(lambda x: np.array(x["racer_name"]))
+courses=pd.DataFrame(columns=['date', 'winners', 'racers'])
+courses['date'] = data.groupby("event_id").apply(lambda x: x["date"].iloc[0])
+courses['racers'] = data.groupby("event_id").apply(lambda x: np.array(x["racer_name"]))
+courses['winners'] = data.groupby("event_id").apply(lambda x: np.repeat(x["racer_name"].values, x["win_lose"]))
 
 racerAux=data.groupby('racer_name')
-racer=pd.DataFrame(columns=['event_id','win_lose'])
+racer=pd.DataFrame(columns=['event_id','win_lose', 'date'])
 racer['win_lose']=racerAux['win_lose'].apply(lambda x:np.array(x))
 racer['event_id']=racerAux.apply(lambda x:np.array(x.index))
- 
+racer['date']=racerAux.apply(lambda x: np.array(x['date']))
 print("Données triées")
+
+## traitement de la date
+def dateFromStr(dateStr):
+    return datetime.datetime(int(dateStr[6:10]), int(dateStr[3:5]), int(dateStr[0:2]), int(dateStr[11:13]), int(dateStr[14:16]))
+
 ##
 def strategie(event_id,nCourse=10):
     try:
-        listRacer=courses.loc[event_id]
+        listRacer=courses['racers'].loc[event_id]
     except TypeError:
-        listRacer=[courses.loc[event_id]]
+        listRacer=[courses['racers'].loc[event_id]]
     maxPerf=0
     bestRacer=listRacer[0]
     for racerName in listRacer:
@@ -71,8 +99,21 @@ def strategie(event_id,nCourse=10):
         indexCourse=np.where(coursesRacer == event_id)[0][0]
         # comme les index sont croissants, on récupère directement le bon nb de courses dispos avant avec indexCourse
         nbCoursesUtilisables = min(indexCourse, nCourse)
-        # initialisation
-        performance = np.sum(resultats[indexCourse - nbCoursesUtilisables:indexCourse])
+        if (nbCoursesUtilisables == 0):
+            continue
+        # Calcul du score du racer
+        #dateNow = dateFromStr(courses['date'].loc[event_id]) # date de la course étudiée
+        #coursesDates = racer.loc[racerName]["date"][indexCourse - nbCoursesUtilisables:indexCourse] # date des courses précédentes
+        #fonc = lambda date: 1/((dateNow - dateFromStr(date)).days+(dateNow - dateFromStr(date)).seconds/24*60*60) # pondérations pour chaque course prop. à proximité temporelle
+        #ponde = np.array([fonc(x) for x in coursesDates])
+        # vecteur de pondération
+        ponde = np.arange(1, nbCoursesUtilisables + 1)**tempFactor
+        # normalisation
+        #print(ponde)
+        ponde = ponde/np.sum(ponde)
+        performance = np.dot(resultats[indexCourse - nbCoursesUtilisables:indexCourse], ponde) # nul si aucune course avant
+        if (resultats[indexCourse - 1] == 1):
+            performance *= factorLastRace
         if (performance>maxPerf):
             maxPerf=performance
             bestRacer=racerName
@@ -86,11 +127,11 @@ def backTest(strat):
         if courses.loc[i].size >= minRacersNb:
             # on parie sur cette course
             nbBets += 1
-            if strat(i) in eventWinner.loc[i]:
+            if strat(i) in courses['winners'].loc[i]:
                 # notre levrier est dans les gagnants
                 w+=1
     return w/nbBets
-##♦
+##
 
 t0 = time.time()
 print(backTest(strategie))
